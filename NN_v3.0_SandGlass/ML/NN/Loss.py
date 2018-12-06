7a7a103e68cd73d7ab19bf85d577191118079058
Hao Chien, Hung <36839106+hchungdelta@users.noreply.github.com>
2018-12-07 05:50:59 +0800

    Update Loss.py

--- NN_v3.0_SandGlass/ML/NN/Loss.py
+++ NN_v3.0_SandGlass/ML/NN/Loss.py
@@ -7,7 +7,26 @@ def square_loss(input_data, target):
     L = 0.5* np.sum((target-input_data)**2)/batch
     All_dLoss = -target + input_data
     return  L, All_dLoss
+def timestep_softmax_cross_entropy(input_data, target):
+    '''
+    input data : shape timestep x batch x depth
+    return prediction(softmax), L, dL
+    '''
+    batch = input_data.shape[1]
 
+    max_value = np.max(input_data, axis=2)
+    tiled_max_value = np.einsum('ij,ijk->ijk', max_value, np.ones_like(input_data))
+    rescale_exp_input = np.exp(input_data-tiled_max_value)
+    sum_of_exp = 1/np.einsum('tbd->tb', rescale_exp_input)
+    after_softmax = np.einsum('tbd,tb->tbd', rescale_exp_input, sum_of_exp)
+
+    #tiled_max_valuhape(input_data.shape).transpose(0,2,1) calculation of L
+    small_num = np.zeros_like(target)
+    small_num.fill(1e-8)   # prevent log(0)
+    L = -np.sum(np.multiply(target, np.log(after_softmax+small_num)))/batch
+    # calculation of dL
+    All_dLoss = -target + after_softmax
+    return after_softmax, L, All_dLoss
 
 def softmax_cross_entropy(input_data, target):
     '''
